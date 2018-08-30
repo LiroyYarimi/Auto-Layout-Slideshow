1af7f709cf2e70aceb0bcdfa88afbcd4d994c656
liroy y <liroy.yarimi@gmail.com>
2018-08-30 14:08:11 +0300

    fix text description and bear image inside uiview

--- Auto-Layout-Slideshow/ViewController.swift
+++ Auto-Layout-Slideshow/ViewController.swift
@@ -22,21 +22,26 @@ class ViewController: UIViewController {
     
     let descriptionTextView : UITextView = {
         let textView = UITextView()
-        textView.text = "Join us today in our fun and games!"
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedStringKey.font : UIFont.boldSystemFont(ofSize: 18)])
+        
+        attributedText.append(NSMutableAttributedString(string: "\n\n\nAre you ready for loads and loads of fun? Don't wait any longer! We hope to see you in our store soon.", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 13), NSAttributedStringKey.foregroundColor : UIColor.gray]))
+        
+        textView.attributedText = attributedText
+        
+        //textView.text = "Join us today in our fun and games!"
+        //textView.font = UIFont.boldSystemFont(ofSize: 18)
         textView.textAlignment = .center
         textView.isEditable = false
         textView.isScrollEnabled = false
         textView.translatesAutoresizingMaskIntoConstraints = false
         return textView
     }()
-//Are you ready for loads and loads of fun? Don't wait any longer! We hope to see you in our store soon.
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(bearImageView) //present bearImage on view
+        //view.addSubview(bearImageView) //present bearImage on view
         view.addSubview(descriptionTextView)
         setupLayout()
         
@@ -53,33 +58,27 @@ class ViewController: UIViewController {
     private func setupLayout(){
         
         let topImageContainerView = UIView()
-        topImageContainerView.backgroundColor = .blue
         view.addSubview(topImageContainerView)
         topImageContainerView.translatesAutoresizingMaskIntoConstraints = false //enable auto-layout
         topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
         //topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         //topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        //this two lines is equal to leftAnchor & rightAnchor 
+        //this two lines is equal to leftAnchor & rightAnchor
         topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         
+        //let's add the bear image view into topImageContainerView
+        topImageContainerView.addSubview(bearImageView)
+        bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
         
         
-        //bearImage.frame = CGRect(x: 0, y: 100, width: 500, height: 500) //this is the old way to do it.
-        
-        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true //center vertical
-        //        bearImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true //center horizontal
-        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true //y=100 pixel from the top of view
-        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true //width size 200
-        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true //height size 200
-        
-        
-        
-        descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 120).isActive = true
+        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
         //descriptionTextView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor,constant:24).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24).isActive = true// rightAnchor need to be negative
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
     }
